b163b44f86f3d28db15d20c33f8236ca5f27201b
Chsa13 <154126449+Chsa13@users.noreply.github.com>
2025-12-13 17:59:59 +0300

    some beauty

--- src/game/ControlsContainer.svelte
+++ src/game/ControlsContainer.svelte
@@ -1,25 +1,29 @@
 <script lang="ts">
   let {onStart, onReset, onCount, square, MaxSquare} = $props();
 </script>
-<button
-  onclick={onStart}
->
-  Начать
-</button>
-<button
-  onclick={onReset}
->
-  Отчистить
-</button>
-<!-- <button
-  onclick={onCount}
->
-  Посчитать
-</button> -->
+<style>
+  .text-cont{
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
 <div>
-  Площадь: {square}
-</div>
-
-<div>
-  Максимальная площадь: {MaxSquare}
+  <button
+    onclick={onStart}
+  >
+    Начать
+  </button>
+  <button
+    onclick={onReset}
+  >
+    Отчистить
+  </button>
+  <div class = "text-cont">
+    <div>
+      Текущая площадь: {square}
+    </div>
+    <div>
+      Возможная площадь: {MaxSquare}
+    </div>
+  </div>
 </div>
