829c78220e781656c6731516f9b2116a1fc3cef1
Chsa13 <154126449+Chsa13@users.noreply.github.com>
2025-12-13 14:20:22 +0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/octoarea/',      // <-- имя репозитория
   build: {
     outDir: 'docs', // вместо dist
   },
